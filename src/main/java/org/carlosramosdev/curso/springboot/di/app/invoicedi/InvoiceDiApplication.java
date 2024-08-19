5c65c41da3b51d3ff7a6791a6535b50acb66fb76
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-19 12:44:14 +0200

    Initial Commit

---
+++ src/main/java/org/carlosramosdev/curso/springboot/di/app/invoicedi/InvoiceDiApplication.java
@@ -0,0 +1,13 @@
+package org.carlosramosdev.curso.springboot.di.app.invoicedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/carlosramosdev/curso/springboot/di/app/invoicedi/InvoiceDiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosramosdev.curso.springboot.di.app.invoicedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
