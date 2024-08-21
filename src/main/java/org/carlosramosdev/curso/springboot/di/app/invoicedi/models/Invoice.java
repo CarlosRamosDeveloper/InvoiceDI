b719ddd0a7d9e2ff8daeffca916407b1da6c7298
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-21 14:29:25 +0200

    models created

---
+++ src/main/java/org/carlosramosdev/curso/springboot/di/app/invoicedi/models/Invoice.java
@@ -0,0 +1,36 @@
+package org.carlosramosdev.curso.springboot.di.app.invoicedi.models;
+
+import java.util.List;
+
+public class Invoice {
+    private Client client;
+    private String description;
+    private List<Item> items;
+
+    public Invoice() {
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/org/carlosramosdev/curso/springboot/di/app/invoicedi/models/Item.java
@@ -0,0 +1,25 @@
+package org.carlosramosdev.curso.springboot.di.app.invoicedi.models;
+
+public class Item {
+    private Product product;
+    private int quantity;
+
+    public Item() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
